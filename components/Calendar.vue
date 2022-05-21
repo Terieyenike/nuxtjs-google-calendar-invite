8b65d5a9250ae8e5d67bb150fdbe8a4e0f617b78
Oteri Eyenike <teyenike1@gmail.com>
2022-05-22 00:43:22 +0100

    Add: component for the invite

---
+++ components/Calendar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <p class="text-2xl">QR code with calendar invite</p>
+    <qrcode :options="{
+      color: {
+        dark: '#FFC72C',
+        light: '#DA291C',
+      },
+      width: 300
+    }" value="https://calendar.google.com/event?action=TEMPLATE&tmeid=X2Nscjc4YmJoZXNyNWtsM2pjcDlqOGkyY2NwMTRpcGEwY2xyNmFyamtlY242b3Q5ZWRsZ2cgdGV5ZW5pa2UxQG0&tmsrc=teyenike1%40gmail.com
+"></qrcode>
+  </div>
+</template>
+
+<script>
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+
+export default {
+  components: {
+    qrcode: VueQrcode,
+  },
+}
+</script>
